4e1f93245ac0496e16cdec5043b6a206809ff50c
Gene Lapointe <gene.lapointe@dbiservices.com>
2016-10-31 22:00:02 -0400

    With Services and Routin

---
+++ app/products/product-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { IProduct } from './product';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: './product-detail.component.html'
+})
+
+export class ProductDetailComponent{
+    pageTitle:string = 'Product Detail';
+    product : IProduct;
+
+    constructor(private _route: ActivatedRoute, 
+                private _router: Router){
+    }
+
+    onBack(): void{
+        this._router.navigate(['/products']);
+    }
+
+    ngOnInit(): void{
+        let id= +this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
+}
